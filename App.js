fbfa71fb12682dddf6bad4b6681bca087229b272
Hossam Samir <Hossameos1@gmail.com>
2017-08-23 18:04:12 +0200

    3 first screens added

--- App.js
+++ App.js
@@ -1,23 +1,114 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native';
+import Carousel from 'react-native-looped-carousel';
 
-export default class App extends React.Component {
+const { width, height } = Dimensions.get('window');
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: { width, height },
+      HospitalNames: ['Hospital #1', 'Hospital #2', 'Hospital #3', 'Hospital #4', 'Hospital #5', 'Hospital #6', 'Hospital #7']
+    };
+  }
+
+  _onLayoutDidChange = (e) => {
+    const layout = e.nativeEvent.layout;
+    this.setState({ size: { width: layout.width, height: layout.height } });
+  }
+
+  // <View>
+  //   <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+  //     <Text style={{ color: 'white', fontWeight: 'bold', }}>value</Text>
+  //   </TouchableHighlight>
+  //   <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+  //     <Text style={{ color: 'white', fontWeight: 'bold', }}>value</Text>
+  //   </TouchableHighlight>
+  // </View>
+
+
+  _Hopitals = (rownum) => {
+    return (
+      this.state.HospitalNames.forEach((hospital) => (
+        <View>
+          <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: 'white', fontWeight: 'bold', }}>hospital</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ color: 'white', fontWeight: 'bold', }}>hospital</Text>
+          </TouchableHighlight>
+        </View>
+      ))
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }} onLayout={this._onLayoutDidChange}>
+        <Carousel
+          style={this.state.size}
+          pageInfo
+          currentPage={1}
+          onAnimateNextPage={(p) => console.log(p)}
+        >
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={require('./assets/img/bg.png')} style={{ width: width, height: height, position: 'absolute' }} />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', }}>
+            <View style={{ flex: .8, backgroundColor: 'transparent' }}>
+              <Image source={require('./assets/img/1.png')} style={{ width: width, height: 210, resizeMode: 'stretch' }} />
+            </View>
+            <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ fontSize: 23, color: '#11284b', fontWeight: 'bold' }}>Choose A Hospital</Text>
+              <ScrollView horizontal={true}>
+                {this._Hopitals()}
+              </ScrollView>
+            </View>
+          </View>
+        </View>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={require('./assets/img/bg.png')} style={{ width: width, height: height, position: 'absolute' }} />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', }}>
+            <View style={{ flex: .8, backgroundColor: 'transparent' }}>
+              <Image source={require('./assets/img/2.png')} style={{ width: width, height: 210, resizeMode: 'stretch' }} />
+            </View>
+            <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ fontSize: 23, color: '#11284b', fontWeight: 'bold' }}>Choose A Date</Text>
+              <ScrollView horizontal={true}>
+                {this._Hopitals()}
+              </ScrollView>
+            </View>
+          </View>
+        </View>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Image source={require('./assets/img/bg.png')} style={{ width: width, height: height, position: 'absolute' }} />
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', }}>
+            <View style={{ flex: .8, backgroundColor: 'transparent' }}>
+              <Image source={require('./assets/img/11.png')} style={{ width: width, height: 210, resizeMode: 'stretch' }} />
+            </View>
+            <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ fontSize: 23, color: '#11284b', fontWeight: 'bold' }}>Choose A Category</Text>
+              <ScrollView horizontal={true}>
+                {this._Hopitals()}
+              </ScrollView>
+            </View>
+          </View>
+        </View>
+
+        </Carousel>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
