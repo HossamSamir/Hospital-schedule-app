c578ae048290849356af8d4945508b1093c49d89
Hossam Samir <Hossameos1@gmail.com>
2017-08-23 18:49:12 +0200

    selecting hospital backend

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Dimensions,
   Image,
   ScrollView,
+  Alert,
   TouchableHighlight
 } from 'react-native';
 import Carousel from 'react-native-looped-carousel';
@@ -18,7 +19,8 @@ export default class App extends Component {
 
     this.state = {
       size: { width, height },
-      HospitalNames: ['Hospital #1', 'Hospital #2', 'Hospital #3', 'Hospital #4', 'Hospital #5', 'Hospital #6', 'Hospital #7']
+      Hospitals: ['Hospital #1', 'Hospital #2', 'Hospital #3', 'Hospital #4', 'Hospital #5', 'Hospital #6', 'Hospital #7'],
+      SelectedHos: ''
     };
   }
 
@@ -38,19 +40,25 @@ export default class App extends Component {
 
 
   _Hopitals = (rownum) => {
-    return (
-      this.state.HospitalNames.forEach((hospital) => (
-        <View>
-          <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ color: 'white', fontWeight: 'bold', }}>hospital</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={{ backgroundColor: '#11284b', margin: 10, padding: 10, borderRadius: 10, height: 40, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ color: 'white', fontWeight: 'bold', }}>hospital</Text>
-          </TouchableHighlight>
-        </View>
-      ))
-    );
+    return this.state.Hospitals.map((Hospital) => (
+      <TouchableHighlight
+         onPress={ () => {this.setState({ SelectedHos: Hospital })} }
+         style={{
+           backgroundColor: Hospital == this.state.SelectedHos ? 'white' : '#11284b',
+           borderColor: '#11284b',
+           borderWidth: 3,
+           margin: 10,
+           padding: 18,
+           borderRadius: 15,
+           height: 50,
+           alignItems: 'center',
+           justifyContent: 'center'
+          }}>
+        <Text style={{ color: Hospital == this.state.SelectedHos ? '#11284b' : 'white', fontWeight: 'bold', fontSize: 15,}}>{ Hospital }</Text>
+      </TouchableHighlight>
+    ))
   }
+
 
   render() {
     return (
@@ -72,10 +80,11 @@ export default class App extends Component {
               <Image source={require('./assets/img/1.png')} style={{ width: width, height: 210, resizeMode: 'stretch' }} />
             </View>
             <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ fontSize: 23, color: '#11284b', fontWeight: 'bold' }}>Choose A Hospital</Text>
-              <ScrollView horizontal={true}>
-                {this._Hopitals()}
+              <Text style={{ fontSize: 28, color: '#11284b', fontWeight: 'bold' }}>Choose A Hospital</Text>
+              <ScrollView horizontal={true} style={{ marginTop: 20 }}>
+                 { this._Hopitals() }
               </ScrollView>
+              <Text>Selected Hospital: {this.state.SelectedHos}</Text>
             </View>
           </View>
         </View>
