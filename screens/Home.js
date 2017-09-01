3dc8202c364cf32147f25a7f738b297065738504
Hossam Samir <Hossameos1@gmail.com>
2017-09-01 05:02:32 +0200

    Hospitals scroll view improvments

--- screens/Home.js
+++ screens/Home.js
@@ -23,7 +23,20 @@ export default class Home extends React.Component {
 
     this.state = {
       size: { width, height },
-      Hospitals: [{name: 'Hospital #1', id: 1}, {name: 'Hospital #2', id: 2}, {name: 'Hospital #3', id: 3}, {name: 'Hospital #4', id: 4}, {name: 'Hospital #5', id: 5}, {name: 'Hospital #6', id: 6}, {name: 'Hospital #7', id: 7}],
+      Hospitals: [
+        [
+          {name: 'Hospital #1', id: 1},
+          {name: 'Hospital #2', id: 2}
+        ],
+        [
+          {name: 'Hospital #3', id: 3},
+          {name: 'Hospital #4', id: 4}
+        ],
+        [
+          {name: 'Hospital #5', id: 5},
+          {name: 'Hospital #6', id: 6}
+        ],
+      ],
       SelectedHos: '',
       date:"2016-05-15",
       Departments: ['Chest', 'Cardiology', 'Diagnostic imaging', 'General surgery', 'Microbiology', 'Occupational therapy'],
@@ -41,27 +54,51 @@ export default class Home extends React.Component {
   };
 
   _Hopitals = () => {
-    return this.state.Hospitals.map((Hospital) => (
-      <TouchableHighlight
-         key={Hospital.id}
-         onPress={ () => {
-           this.setState({ SelectedHos: Hospital.name })
-           this.props.navigation.navigate('Chat', { name: Hospital.name })
-         }}
-         style={{
-           backgroundColor: Hospital.name == this.state.SelectedHos ? 'white' : '#11284b',
-           borderColor: '#11284b',
-           borderWidth: 3,
-           margin: 10,
-           padding: 18,
-           borderRadius: 15,
-           height: 50,
-           alignItems: 'center',
-           justifyContent: 'center'
-          }}>
-        <Text style={{ color: Hospital.name == this.state.SelectedHos ? '#11284b' : 'white', fontWeight: 'bold', fontSize: 15,}}>{ Hospital.name }</Text>
-      </TouchableHighlight>
-    ))
+    return this.state.Hospitals.map((Hospital, i) => {
+      return (
+        <View key={Hospital[0].id} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+          <View style={{ flex: 1  }}>
+            <TouchableHighlight
+               onPress={ () => {
+                 this.setState({ SelectedHos: Hospital[0].name })
+                 this.props.navigation.navigate('Chat', { name: Hospital[0].name })
+               }}
+               style={{
+                 backgroundColor: Hospital[0].name == this.state.SelectedHos ? 'white' : '#11284b',
+                 borderColor: '#11284b',
+                 borderWidth: 3,
+                 margin: 10,
+                 padding: 18,
+                 borderRadius: 15,
+                 alignItems: 'center',
+                 justifyContent: 'center'
+                }}>
+              <Text style={{ color: Hospital[0].name == this.state.SelectedHos ? '#11284b' : 'white', fontWeight: 'bold', fontSize: 15,}}>{ Hospital[0].name }</Text>
+            </TouchableHighlight>
+          </View>
+
+          <View style={{ flex: 1  }}>
+            <TouchableHighlight
+               onPress={ () => {
+                 this.setState({ SelectedHos: Hospital[1].name })
+                 this.props.navigation.navigate('Chat', { name: Hospital[1].name })
+               }}
+               style={{
+                 backgroundColor: Hospital[1].name == this.state.SelectedHos ? 'white' : '#11284b',
+                 borderColor: '#11284b',
+                 borderWidth: 3,
+                 margin: 10,
+                 padding: 18,
+                 borderRadius: 15,
+                 alignItems: 'center',
+                 justifyContent: 'center'
+                }}>
+              <Text style={{ color: Hospital[1].name == this.state.SelectedHos ? '#11284b' : 'white', fontWeight: 'bold', fontSize: 15,}}>{ Hospital[1].name }</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      )
+    })
   }
 
   render() {
@@ -77,7 +114,7 @@ export default class Home extends React.Component {
             <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
               <Text style={{ fontSize: 28, color: '#11284b', fontWeight: 'bold' }}>Choose A Hospital</Text>
               <Text style={{ color: '#11284b', opacity: .8, fontWeight: 'bold' }}>Selected Hospital: {this.state.SelectedHos}</Text>
-              <ScrollView horizontal={true} style={{ marginTop: 30, backgroundColor: 'transparent' }}>
+              <ScrollView style={{ flex: 1, marginTop: 30, backgroundColor: 'transparent', width }}>
 
                 {this._Hopitals()}
 
