6305dec315b14cdd30776ca3de6f8bca559b5c98
Hossam Samir <Hossameos1@gmail.com>
2017-09-03 06:28:53 +0200

    feedback modal

--- screens/About.js
+++ screens/About.js
@@ -9,6 +9,9 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  Modal,
+  TextInput,
+  Alert
  } from 'react-native';
 import {
   StackNavigator,
@@ -22,7 +25,15 @@ export default class About extends React.Component {
     super(props);
     this.state = {
       size: { width, height },
+      modalVisible: false,
+      name: '',
+      email: '',
+      message: '',
     };
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
   }
 
   _onLayoutDidChange = (e) => {
@@ -40,6 +51,9 @@ export default class About extends React.Component {
     return (
       <View style={{ flex: 1, }} onLayout={this._onLayoutDidChange}>
         <Image source={require('../assets/img/bg-icons.png')} style={{ width, height, position: 'absolute' }} />
+        <TouchableOpacity style={{marginTop: 20, marginLeft: 10, position: 'absolute', zIndex: 2}} onPress={ () => this.setModalVisible(true) }>
+          <Image source={require('../assets/img/icon.png')} style={{ width: 45, height: 45, }} />
+        </TouchableOpacity>
         <View style={{ flex: 1 }}>
           <Image style={{ flex: .2 }} source={require('../assets/img/illustration.png')} style={{ width, height: 250, }} />
           <View style={{ flex: 2, alignItems: 'center' }}>
@@ -54,6 +68,53 @@ export default class About extends React.Component {
             </TouchableHighlight>
           </View>
         </View>
+
+
+        <Modal
+          style={{ flex: 1, backgroundColor: 'transparent' }}
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+
+          <TouchableHighlight
+            style={{ flex: 1, backgroundColor: 'transparent' }}
+            onPress={() => {
+            this.setModalVisible(!this.state.modalVisible)
+          }}>
+            <Text></Text>
+          </TouchableHighlight>
+
+           <View style={{marginTop: 22, flex: 4}}>
+             <Image source={require('../assets/img/contact.png')} style={{ width, height, position: 'absolute', }} />
+               <TouchableHighlight style={{marginTop: 20, marginLeft: 10, position: 'absolute', zIndex: 2}} onPress={ () => this.setModalVisible(!this.state.modalVisible) }>
+                 <Image source={require('../assets/img/close.png')} style={{ width: 45, height: 45, marginLeft: '83%'}} />
+               </TouchableHighlight>
+            <ScrollView style={{ marginHorizontal: 20, marginTop: 45 }}>
+
+              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 50, }}
+                onChangeText={(name) => this.setState({ name })}
+                placeholder="Name" />
+              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 50, }}
+                onChangeText={(email) => this.setState({ email })}
+                placeholder="email" />
+              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 100, }}
+                onChangeText={(message) => this.setState({ message })}
+                placeholder="message" />
+              <TouchableOpacity
+                onPress={ () => {
+                  Alert.alert('Thanks for your feedback' + this.state.name + ' :)', 'we will responde soon')
+                }}
+                 style={{ backgroundColor: '#11284b', borderWidth: 2, borderColor: '#11284b', borderRadius: 20, width: '80%' }}>
+                <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', padding: 18 }}>Send</Text>
+              </TouchableOpacity>
+            </ScrollView>
+
+         </View>
+        </Modal>
+
+
       </View>
     );
   }
