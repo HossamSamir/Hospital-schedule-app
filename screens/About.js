229228acbd108319a37c8fb18821d7f4e47ea713
Hossam Samir <Hossameos1@gmail.com>
2017-09-06 07:50:05 +0200

    fix a typo

--- screens/About.js
+++ screens/About.js
@@ -55,7 +55,7 @@ export default class About extends React.Component {
           <Image style={{ flex: .2 }} source={require('../assets/img/illustration.png')} style={{ width, height: 200, resizeMode: 'contain', marginTop: -23}} />
           <View style={{ flex: 2, }}>
               <Text style={{ fontSize: 15, lineHeight: 25, marginHorizontal: 20, marginTop: 20, fontWeight: 'bold', color: '#11284b' }}>
-                OnCall is a application which provides information about doctors Oncall in hospitals and facilitates easy access to doctors in all sections of the hospital. Also it presents  details about team on duty for each day. The goal is to reduce the time to search and reduce the cost of printing and distributing the papers. The application is available on the Apple and Android stores, The download is free.
+                OnCall is an application which provides information about doctors Oncall in hospitals and facilitates easy access to doctors in all sections of the hospital. Also it presents  details about team on duty for each day. The goal is to reduce the time to search and reduce the cost of printing and distributing the papers. The application is available on the Apple and Android stores, The download is free.
                 This work is personal and does not belong to any organization.
                 Fees may be charged for uploading later.
                 To contact us send your request to join  the team or in case to add your hospital on our App , Please send  via the contact us button .
