6bb317690a5879189b812f1e84935943e8f5b55d
Hossam Samir <Hossameos1@gmail.com>
2017-09-06 19:46:59 +0200

    he doesnt like the home screen :3

--- screens/Home.js
+++ screens/Home.js
@@ -49,7 +49,8 @@ export default class Home extends React.Component {
         <Image source={require('../assets/img/bg-icons.png')} style={{ width, height, position: 'absolute' }} />
 
         <View style={{ flex: .8, alignItems: 'center', justifyContent: 'center' }}>
-          <Image source={require('../assets/img/o.png')} style={{ flex: 1, alignSelf: 'stretch', width: undefined, height: undefined, margin: 40, marginBottom: 10, resizeMode: 'contain'}} />
+            <Image style={{ flex: .2 }} source={require('../assets/img/illustration.png')} style={{ width, height: 200, resizeMode: 'contain', marginTop: -23}} />
+            <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center', marginTop: 40, color: '#11284b' }}>you are too busy to think about schedules, Relax and let us handle it for you :)</Text>
         </View>
 
         <View style={{ flex: 1, flexDirection: 'row',}}>
@@ -63,7 +64,10 @@ export default class Home extends React.Component {
             shadowOpacity: 0.5,
             shadowRadius: 2,
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            borderColor: '#11284b',
+            borderWidth: 1,
+            borderRadius: 10
            }}>
            <Image source={require('../assets/img/about.png')} style={{ flex: 1, alignSelf: 'stretch', width: undefined, height: undefined, margin: 50, marginBottom: 0, resizeMode: 'contain'}} />
            <Text style={{flex: 1, marginTop: 10, fontSize: 18, fontWeight: 'bold', color: '#1e537d'}}>About us</Text>
@@ -79,7 +83,10 @@ export default class Home extends React.Component {
            shadowOpacity: 0.5,
            shadowRadius: 2,
            alignItems: 'center',
-           justifyContent: 'center'
+           justifyContent: 'center',
+           borderColor: '#11284b',
+           borderWidth: 1,
+           borderRadius: 10
           }}>
           <Image source={require('../assets/img/contact-us.png')} style={{ flex: 1, alignSelf: 'stretch', width: undefined, height: undefined, margin: 50, marginBottom: 0, resizeMode: 'contain'}} />
           <Text style={{flex: 1, marginTop: 10, fontSize: 18, fontWeight: 'bold', color: '#1e537d'}}>Contact us</Text>
