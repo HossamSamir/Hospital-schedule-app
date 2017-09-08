2ae6b7528ea1109cd51dd00e5b9df877684d305b
Hossam Samir <Hossameos1@gmail.com>
2017-09-08 02:08:49 +0200

    fetching next day doctors

--- screens/Results.js
+++ screens/Results.js
@@ -9,11 +9,13 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  Alert
  } from 'react-native';
 import {
   StackNavigator,
 } from 'react-navigation';
 import Timeline from 'react-native-timeline-listview';
+import { EvilIcons, Ionicons } from '@expo/vector-icons';
 
 const { width, height } = Dimensions.get('window');
 
@@ -25,9 +27,7 @@ export default class Results extends React.Component {
   }
 
   getDayName() {
-    console.log('=================================');
-    console.log(this.props.navigation.state.params.Date);
-    var d = new Date(this.props.navigation.state.params.Date);
+    var d = new Date(this.state.Date);
     var dayName = this.state.week[d.getDay()];
     this.setState({ DayToShow: dayName })
   }
@@ -39,7 +39,7 @@ export default class Results extends React.Component {
     .then((resJson) => {
       resJson.map((doc) => {
         if (doc.title != '') {
-          this.data.push(doc)
+          this.state.data.push(doc)
         }
       })
     })
@@ -48,16 +48,40 @@ export default class Results extends React.Component {
     })
   }
 
+  _GoNext = () => {
+    this.setState({data: []})
+
+    fetch(
+      `https://oncall-admin.herokuapp.com/api/forward?section_id=${this.props.navigation.state.params.Department.id}&hospital_id=${this.props.navigation.state.params.Hospital.id}&date=${this.state.Date}`)
+    .then((res) => res.json())
+    .then((resJson) => {
+      resJson.map((doc) => {
+        if (doc.title != '') {
+          this.state.data.push(doc)
+          this.setState({Date: doc.date})
+        }
+      })
+    })
+    .then(() => {
+      this.setState({doneFetching: true, DayNum: this.state.DayNum+1})
+    })
+    .then(() => {
+      this.getDayName()
+    })
+  }
 
   constructor(props) {
     super(props);
     this.renderDetail = this.renderDetail.bind(this)
-    this.data = [];
     this.state = {
       size: { width, height },
       doneFetching: false,
+      data: [],
       week: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      DayToShow: 'test'
+      DayToShow: 'test',
+      DepartmentName: this.props.navigation.state.params.Department.name,
+      DayNum: this.props.navigation.state.params.DayNum,
+      Date: this.props.navigation.state.params.Date
     };
   }
 
@@ -96,8 +120,6 @@ export default class Results extends React.Component {
     }
   }
 
-
-
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -107,13 +129,27 @@ export default class Results extends React.Component {
 
       <View style={{ }}>
         <Image source={require('../assets/img/results.png')} style={{ width, height: 200, position: 'absolute'}} />
-        <View style={{ backgroundColor: 'transparent', width, marginTop: 30, marginLeft: 30, height: 180 }}>
-          <Text style={{ color: 'white', fontSize: 60, }}>
-            {this.props.navigation.state.params.DayNum}
-            <Text style={{ color: 'white', fontSize: 30, }}>  {this.state.DayToShow}</Text>
-          </Text>
-          <Text style={{ color: 'white', fontSize: 30, marginLeft: 13 }}>{this.props.navigation.state.params.Department.name}</Text>
+        <View style={{ flexDirection: 'row', backgroundColor: 'transparent', width, height: 200 }}>
+
+          <View style={{ flex: .2, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity onPress={ this._GoPrev }>
+              <Ionicons name='ios-arrow-dropleft-circle' size={50} color='#c3c3c3' style={{ backgroundColor: 'transparent',  }} />
+            </TouchableOpacity>
         </View>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: 'white', fontSize: 60, }}>
+              {this.state.DayNum}
+              <Text style={{ color: 'white', fontSize: 30, }}>  {this.state.DayToShow}</Text>
+            </Text>
+            <Text style={{ color: 'white', fontSize: 30, marginLeft: 13 }}>{this.state.DepartmentName}</Text>
+          </View>
+          <View style={{ flex: .2, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity onPress={ this._GoNext }>
+              <Ionicons name='ios-arrow-dropright-circle' size={50} color='#c3c3c3' style={{ backgroundColor: 'transparent',}} />
+            </TouchableOpacity>
+        </View>
+
+      </View>
       </View>
 
       <Timeline
@@ -125,7 +161,7 @@ export default class Results extends React.Component {
         timeContainerStyle={{minWidth:75,}}
         timeStyle={{borderWidth: 2, borderColor: '#1e537d', color: '#1e537d', fontWeight: 'bold', borderRadius: 10, padding: 5, textAlign: 'center'}}
         renderDetail={this.renderDetail}
-        data={this.data} />
+        data={this.state.data} />
 
       </View>
     );
