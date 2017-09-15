aa58429d1463d2a82d38b08a322d9db558c38b1e
Hossam Samir <Hossameos1@gmail.com>
2017-09-15 08:58:22 +0200

    preformance imporvments in about us screen

--- screens/About.js
+++ screens/About.js
@@ -1,51 +1,29 @@
 import React from 'react';
 import {
-  StyleSheet,
   ScrollView,
   Text,
-  TouchableHighlight,
   View,
-  Button,
   Image,
-  TouchableOpacity,
   Dimensions,
-  Modal,
-  TextInput,
-  Alert
  } from 'react-native';
 import {
   StackNavigator,
 } from 'react-navigation';
-
 const { width, height } = Dimensions.get('window');
-
 export default class About extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       size: { width, height },
-      modalVisible: false,
-      name: '',
-      email: '',
-      message: '',
     };
   }
-
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
-  }
-
-  _onLayoutDidChange = (e) => {
+  _onLayoutDidChange = function() {
     const layout = e.nativeEvent.layout;
     this.setState({ size: { width: layout.width, height: layout.height } });
   }
-
   static navigationOptions = {
     title: 'About us'
   };
-
-
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -67,63 +45,7 @@ export default class About extends React.Component {
               </Text>
           </View>
         </ScrollView>
-
-
-        <Modal
-          style={{ flex: 1, backgroundColor: 'transparent' }}
-          animationType="slide"
-          transparent={true}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {alert("Modal has been closed.")}}
-          >
-
-          <TouchableHighlight
-            style={{ flex: 1, backgroundColor: 'transparent' }}
-            onPress={() => {
-            this.setModalVisible(!this.state.modalVisible)
-          }}>
-            <Text></Text>
-          </TouchableHighlight>
-
-           <View style={{marginTop: 22, flex: 4}}>
-             <Image source={require('../assets/img/contact.png')} style={{ width, height, position: 'absolute', }} />
-               <TouchableHighlight style={{marginTop: 20, marginLeft: 10, position: 'absolute', zIndex: 2}} onPress={ () => this.setModalVisible(!this.state.modalVisible) }>
-                 <Image source={require('../assets/img/close.png')} style={{ width: 45, height: 45, marginLeft: '83%'}} />
-               </TouchableHighlight>
-            <ScrollView style={{ marginHorizontal: 20, marginTop: 45 }}>
-
-              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 50, }}
-                onChangeText={(name) => this.setState({ name })}
-                placeholder="Name" />
-              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 50, }}
-                onChangeText={(email) => this.setState({ email })}
-                placeholder="email" />
-              <TextInput style={{ borderColor: '#11284b', borderWidth: 2, marginVertical: 7, borderRadius: 20, width: '80%', textAlign: 'center', height: 100, }}
-                onChangeText={(message) => this.setState({ message })}
-                placeholder="message" />
-              <TouchableOpacity
-                onPress={ () => {
-                  Alert.alert('Thanks for your feedback' + this.state.name + ' :)', 'we will responde soon')
-                }}
-                 style={{ backgroundColor: '#11284b', borderWidth: 2, borderColor: '#11284b', borderRadius: 20, width: '80%' }}>
-                <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', padding: 18 }}>Send</Text>
-              </TouchableOpacity>
-            </ScrollView>
-
-         </View>
-        </Modal>
-
-
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
