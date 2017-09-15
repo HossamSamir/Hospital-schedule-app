1b6cc6d15a14d838ad6d2f84a61efae118a0d0b0
Hossam Samir <Hossameos1@gmail.com>
2017-09-15 09:20:28 +0200

    preformance imporvments in the home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,53 +1,32 @@
 import React from 'react';
 import {
-  StyleSheet,
-  ScrollView,
   Text,
   TouchableHighlight,
   View,
-  Button,
   Image,
   TouchableOpacity,
   Dimensions,
-  Modal,
-  TextInput,
-  Alert
  } from 'react-native';
-import {
-  StackNavigator,
-} from 'react-navigation';
-
 const { width, height } = Dimensions.get('window');
-
 export default class Home extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       size: { width, height },
     };
   }
-
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
-  }
-
   _onLayoutDidChange = (e) => {
     const layout = e.nativeEvent.layout;
     this.setState({ size: { width: layout.width, height: layout.height } });
   }
-
   static navigationOptions = {
     header: null
   };
-
-
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={{ flex: 1, backgroundColor: 'white'}} onLayout={this._onLayoutDidChange}>
         <Image source={require('../assets/img/bg-logo.png')} style={{ width, height, position: 'absolute' }} />
-
         <View style={{ flex: .8, alignItems: 'center', justifyContent: 'center' }}>
             <Image style={{ flex: .2 }} source={require('../assets/img/illustration.png')} style={{ width, height: 200, resizeMode: 'contain', marginTop: -23}} />
         </View>
@@ -75,7 +54,6 @@ export default class Home extends React.Component {
               <Image source={require('../assets/img/about.png')} style={{ width: 50, height: 50, margin: 5 }} />
              <Text style={{fontSize: 18, fontWeight: 'bold', color: '#1e537d', margin: 5}}>About us</Text>
          </TouchableOpacity>
-
          <TouchableOpacity
            onPress={() => {
              navigate('Contact')
@@ -99,7 +77,6 @@ export default class Home extends React.Component {
           <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#1e537d', margin: 5 }}>Contact us</Text>
         </TouchableOpacity>
       </View>
-
         <TouchableHighlight
           onPress={ () => {
             navigate('Hospitals')
@@ -107,11 +84,7 @@ export default class Home extends React.Component {
           style={{ flex: .2, backgroundColor: '#11284b', justifyContent: 'center', alignItems: 'center' }}>
           <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, marginVertical: 8 }}>Get started</Text>
         </TouchableHighlight>
-
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-});
