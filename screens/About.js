67060b9298524996c3e776c06e26a176ddac3b03
Hossam Samir <Hossameos1@gmail.com>
2017-09-27 14:13:08 +0200

    about screen improvements..

--- screens/About.js
+++ screens/About.js
@@ -4,14 +4,30 @@ import {
   Text,
   View,
   Image,
+  FlatList,
   Dimensions,
  } from 'react-native';
+ import { List, ListItem, } from 'react-native-elements';
 const { width, height } = Dimensions.get('window');
 export default class About extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       size: { width, height },
+      users: [
+        {
+          txt: 'OnCall is an application which provides information about doctors Oncall in hospitals and facilitates easy access to doctors in all sections of the hospital. Also it presents  details about team on duty for each day. The goal is to reduce the time to search and reduce the cost of printing and distributing the papers. The application is available on the Apple and Android stores, The download is free. This work is personal and does not belong to any organization. Fees may be charged for uploading later. To contact us send your request to join  the team or in case to add your hospital on our App , Please send  via the contact us button . do not hestiate for any question , suggestion or request .',
+          id: 1
+        },
+        {
+          txt: 'Best Regard,',
+          id: 2
+        },
+        {
+          txt: 'Team Work.',
+          id: 3
+        }
+    ],
     };
   }
   _onLayoutDidChange = (e) => {
@@ -24,23 +40,26 @@ export default class About extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, }} onLayout={this._onLayoutDidChange}>
-        <Image source={require('../assets/img/bg-icons.png')} style={{ width, height, position: 'absolute' }} />
-        <ScrollView style={{ flex: 1 }}>
-          <Image style={{ flex: .2 }} source={require('../assets/img/illustration.png')} style={{ width, height: 200, resizeMode: 'contain', marginTop: -23}} />
-          <View style={{ flex: 2, }}>
-              <Text style={{ fontSize: 15, lineHeight: 25, marginHorizontal: 20, marginTop: 20, fontWeight: 'bold', color: '#11284b' }}>
-                OnCall is an application which provides information about doctors Oncall in hospitals and facilitates easy access to doctors in all sections of the hospital. Also it presents  details about team on duty for each day. The goal is to reduce the time to search and reduce the cost of printing and distributing the papers. The application is available on the Apple and Android stores, The download is free.
-                This work is personal and does not belong to any organization.
-                Fees may be charged for uploading later.
-                To contact us send your request to join  the team or in case to add your hospital on our App , Please send  via the contact us button .
-                do not hestiate for any question , suggestion or request .
-              </Text>
-              <Text style={{ fontSize: 15, lineHeight: 25, marginHorizontal: 20, marginTop: 10, fontWeight: 'bold', color: '#11284b', textAlign: 'left' }}>
-                Best Regard,
-                Team Work.
-              </Text>
-          </View>
-        </ScrollView>
+        <Image source={require('../assets/img/bg-icons.jpg')} style={{ width, height, position: 'absolute' }} />
+
+
+
+
+          <List containerStyle={{flex: 1, marginTop: 250, flexDirection: 'row', borderTopWidth: 0}}>
+            <FlatList
+              data = {this.state.users}
+              renderItem = {({ item }) => (
+                <Text style={{ fontSize: 15, lineHeight: 25, marginHorizontal: 20, fontWeight: 'bold', color: '#11284b' }}>{item.txt}</Text>
+              )}
+              keyExtractor={item => item.id}
+              ListHeaderComponent = {this._renderHeader}
+            />
+          </List>
+
+
+
+
+
       </View>
     );
   }
